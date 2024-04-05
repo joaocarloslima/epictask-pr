9312a93de8cac2375ef06b63b09232115848b535
joaocarloslima <joaocarloslima@me.com>
2024-04-04 22:52:58 -0300

    validation, i18n

--- src/main/java/br/com/fiap/epictask/task/Task.java
+++ src/main/java/br/com/fiap/epictask/task/Task.java
@@ -4,6 +4,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,9 +21,17 @@ public class Task {
     
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank(message = "{task.title.notblank}")
     private String title;
+
+    @Size(min = 10, max = 255, message = "{task.description.size}")
     private String description;
+
+    @Positive(message = "{task.score.positive}" )
     private int score;
+
+    @Min(0) @Max(100)
     private int status;
 
 }
--- src/main/java/br/com/fiap/epictask/task/TaskController.java
+++ src/main/java/br/com/fiap/epictask/task/TaskController.java
@@ -1,17 +1,19 @@
 package br.com.fiap.epictask.task;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("tasks")
@@ -20,13 +22,16 @@ public class TaskController {
     @Autowired
     TaskRepository repository;
 
+    @Autowired
+    MessageSource messageSource;
+
     @GetMapping
     public String index(Model model){
         model.addAttribute("tasks", repository.findAll());
         return "task/index";
     }
 
-    @GetMapping("delete/{id}")
+    @DeleteMapping("{id}")
     public String delete(@PathVariable Long id, RedirectAttributes redirect){
         var task = repository.findById(id);
 
@@ -36,17 +41,19 @@ public class TaskController {
         }
         
         repository.deleteById(id);
-        redirect.addFlashAttribute("message", "Tarefa apagada com sucesso");
+        redirect.addFlashAttribute("message", messageSource.getMessage("task.delete", null, LocaleContextHolder.getLocale()));
         return "redirect:/tasks";
     }
     
     @GetMapping("new")
-    public String form(){
+    public String form(Task task){
         return "task/form";
     }
     
     @PostMapping
-    public String create(Task task, RedirectAttributes redirect){
+    public String create(@Valid Task task, BindingResult result, RedirectAttributes redirect){
+        if (result.hasErrors()) return "task/form";
+
         repository.save(task);
         redirect.addFlashAttribute("message", "Tarefa cadastrada com sucesso");
         return "redirect:/tasks";
